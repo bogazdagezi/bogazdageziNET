1fa9b7e28c03daca56694fe67fe365aeb93cf016
bogazdagezi <127502821+bogazdagezi@users.noreply.github.com>
2023-03-10 15:19:41 +0300

    Update root.tsx

--- src/root.tsx
+++ src/root.tsx
@@ -27,6 +27,7 @@ export default function Root() {
           <ErrorBoundary>
             <A href="/">Index</A>
             <A href="/about">About</A>
+            <A href="https://www.bogazdagezi.net">Web site</A>
             <Routes>
               <FileRoutes />
             </Routes>
